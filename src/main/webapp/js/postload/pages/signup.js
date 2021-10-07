ba130851c81507849c02edf26cf358262fc960e8
ryankwei <rkwei@usc.edu>
2021-10-06 20:08:44 -0700

    Add acceptance tests and remove console.logs

--- src/main/webapp/js/postload/pages/signup.js
+++ src/main/webapp/js/postload/pages/signup.js
@@ -2,7 +2,5 @@ document.querySelector(".on-enter-target").onclick = function(e) {
     e.preventDefault();
     let username = document.querySelector("#input-username").value;
     let password = document.querySelector("#input-password").value;
-    console.log(username);
-    console.log(password);
     signup(username, password);
 }
