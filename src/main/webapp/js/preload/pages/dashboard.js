d2ac7d04379b313d462735dccfbca590c1a73fa4
Jack Mandelkorn <jack@jackmandelkorn.com>
2021-10-21 12:27:48 -0700

    adding country code selector to search events ui

--- src/main/webapp/js/preload/pages/dashboard.js
+++ src/main/webapp/js/preload/pages/dashboard.js
@@ -5,8 +5,10 @@ const logout = () => {
 
 const search = () => {
   const keywords = document.getElementById("keywords")
+  const country = document.getElementById("country")
   const startDate = document.getElementById("start-date")
   const endDate = document.getElementById("end-date")
+  const code = country.value.trim().toUpperCase()
   if (keywords.value.length < 1) {
     $(keywords).addClass("invalid")
     genResults()
@@ -15,43 +17,66 @@ const search = () => {
   else {
     $(keywords).removeClass("invalid")
   }
+  let add = ""
+  if (COUNTRIES[code]) {
+    add = ("&countryCode=" + code)
+  }
+  $("#search-button").addClass("loading")
   if (startDate.value.length && endDate.value.length) {
-    ajaxGet(ENDPOINT_URL + "/search/event?keyword=" + encodeURIComponent(keywords.value.trim()) + "&startDate=" + startDate.value + "&endDate=" + endDate.value, (response) => {
+    ajaxGet(ENDPOINT_URL + "/search/event?keyword=" + encodeURIComponent(keywords.value.trim()) + "&startDate=" + startDate.value + "&endDate=" + endDate.value + add, (response) => {
       let json = JSON.parse(response)
       genResults(json)
+      $("#search-button").removeClass("loading")
     })
   }
   else {
-    ajaxGet(ENDPOINT_URL + "/search/event?keyword=" + encodeURIComponent(keywords.value.trim()), (response) => {
+    ajaxGet(ENDPOINT_URL + "/search/event?keyword=" + encodeURIComponent(keywords.value.trim()) + add, (response) => {
       let json = JSON.parse(response)
       genResults(json)
+      $("#search-button").removeClass("loading")
     })
   }
 }
 
-
 const genResults = (json = {}) => {
   const container = document.getElementById("results")
   container.innerHTML = ""
-  if (JSON.stringify(json) === "{}" || !json.status) {
-    let p = document.createElement("p")
-    p.className = "error"
-    p.innerHTML = "No results."
-    container.appendChild(p)
+  if (JSON.stringify(json) !== "{}") {
+    if (!json.status) {
+      let msg = document.createElement("p")
+      msg.className = "error"
+      msg.innerHTML = "No results"
+      container.appendChild(msg)
+    }
+    else {
+      let msg = document.createElement("p")
+      msg.innerHTML = (json.data.events.length.toString() + " result(s)").trim()
+      container.appendChild(msg)
+      for (const event of json.data.events) {
+        let result = document.createElement("div")
+        result.className = "result"
+        let p = document.createElement("p")
+        p.className = "subtitle"
+        p.innerHTML = moment(event.date).format("MMMM Do YYYY")
+        let a = document.createElement("a")
+        a.href = event.url
+        a.innerHTML = event.name
+        result.appendChild(p)
+        result.appendChild(a)
+        container.appendChild(result)
+      }
+    }
+  }
+}
+
+const country = () => {
+  const element = document.getElementById("country")
+  element.value = element.value.trim().toUpperCase()
+  const code = element.value
+  if (COUNTRIES[code]) {
+    element.style.backgroundImage = ("url('https://flagcdn.com/h60/" + code.toLowerCase() + ".png')")
   }
   else {
-    for (const event of json.data.events) {
-      let result = document.createElement("div")
-      result.className = "result"
-      let p = document.createElement("p")
-      p.className = "subtitle"
-      p.innerHTML = moment(event.date).format("MMMM Do YYYY")
-      let a = document.createElement("a")
-      a.href = event.url
-      a.innerHTML = event.name
-      result.appendChild(p)
-      result.appendChild(a)
-      container.appendChild(result)
-    }
+    element.style.backgroundImage = "url('../../assets/images/dummy-flag.png')"
   }
 }
