78aec467e88d60bcf234535154f15606f8620772
ryankwei <rkwei@usc.edu>
2021-10-21 20:00:00 -0700

    Refactor javascript to be more reusable

---
+++ src/main/webapp/js/postload/pages/dashboard.js
@@ -0,0 +1,17 @@
+document.querySelector("#country").onpaste = () => {
+    const element = document.getElementById("country");
+    country(element);
+}
+
+document.querySelector("#search-button").onclick = (e) => {
+    e.preventDefault()
+    const keywords = document.getElementById("keywords")
+    const country = document.getElementById("country")
+    const startDate = document.getElementById("start-date")
+    const endDate = document.getElementById("end-date")
+    const errMsg = document.querySelector(".error-msg")
+    const code = country.value.trim().toUpperCase()
+    const container = document.getElementById("results")
+
+    search(keywords, country, startDate, endDate, errMsg, code, container);
+}
