09799b2bcdd6bfc589871eeae684121c8fa7e001
Andre Hsueh <andrehsu@usc.edu>
2021-11-16 21:13:39 -0500

    Update proposalResponse.js

--- src/main/webapp/js/postload/pages/proposalResponse.js
+++ src/main/webapp/js/postload/pages/proposalResponse.js
@@ -8,6 +8,63 @@ const logout = () => {
 ajaxGet(ENDPOINT_URL + "/proposal/get?type=received&username=" + "inviteeNametester1", (response) => {
         let json = JSON.parse(response);
       });
+      const genResults = (json = {}, container) => {
+       container.empty();
+       if (JSON.stringify(json) !== "{}") {
+         if (!json.status) {
+           let msg = document.createElement("p")
+           msg.className = "error"
+           msg.innerHTML = "No results found"
+           container.appendChild(msg)
+         }
+         else {
+           for (let i=0;i<json.data.length;i++) {
+             let str = `<li class="list-group-item">
+                                        <div class="row">
+                                            <div class="col-2">
+                                                <strong>${json.data[i].proposalTitle}</strong>
+                                                <br>
+                                                Event Name: ${json.data[i].events[0].name}
+                                                <br>
+                                                Date: ${json.data[i].events[0].date}
+                                                <br>
+                                                Time: ${json.data[i].events[0].time}
+                                                <br>
+                                                                                          URL: ${json.data[i].events[0].url}
+                                                                                          <br>
+                                                                                                                                    Genre: ${json.data[i].events[0].genre}
+                                        </div>
+                                            <div class="col-3">
+                                                <input type="radio" class="btn-check" name="yes-no${i}" id="yes1${i}" autocomplete="off">
+                                            </div>
+                                            <div class="col-2">
+                                                <label class="btn btn-outline-success" name="yes-no${i}" for="yes${i}">Yes</label>
+                                            </div>
+                                            <div class="col-3">
+                                            <input type="radio" class="btn-check" name="yes-no${i}" id="no${i}" autocomplete="off">
+                                        </div>
+                                            <div class="col-2">
+                                                <label class="btn btn-outline-success" name="yes-no${i}" for="no${i}">No</label>
+                                                <br><br><br>
+                                                Excitement:
+                                                <select id="${i}">
+                                                <option selected disabled hidden style='display: none' value=''></option>
+                                                <option value="1">1</option>
+                                                <option value="2">2</option>
+                                                <option value="3">3</option>
+                                                <option value="4">4</option>
+                                                <option value="5">5</option>
+                                                </select>
+                                            </div>
+      
+                                        </div>
+                                    </li>`;
+           container.append(str);
+            console.log("HI");
+           }
+         }
+       }
+       }
 const search = (username, resultsContainer) => {
 // ajaxGet(ENDPOINT_URL + "/proposal/get?type=received&username=" + "inviteeNametester1", (response) => {
 //       let json = JSON.parse(response)
@@ -16,63 +73,6 @@ const search = (username, resultsContainer) => {
 
 };
  search(username,c);
-const genResults = (json = {}, container) => {
- container.empty();
- if (JSON.stringify(json) !== "{}") {
-   if (!json.status) {
-     let msg = document.createElement("p")
-     msg.className = "error"
-     msg.innerHTML = "No results found"
-     container.appendChild(msg)
-   }
-   else {
-     for (let i=0;i<json.data.length;i++) {
-       let str = `<li class="list-group-item">
-                                  <div class="row">
-                                      <div class="col-2">
-                                          <strong>${json.data[i].proposalTitle}</strong>
-                                          <br>
-                                          Event Name: ${json.data[i].events[0].name}
-                                          <br>
-                                          Date: ${json.data[i].events[0].date}
-                                          <br>
-                                          Time: ${json.data[i].events[0].time}
-                                          <br>
-                                                                                    URL: ${json.data[i].events[0].url}
-                                                                                    <br>
-                                                                                                                              Genre: ${json.data[i].events[0].genre}
-                                  </div>
-                                      <div class="col-3">
-                                          <input type="radio" class="btn-check" name="yes-no${i}" id="yes1${i}" autocomplete="off">
-                                      </div>
-                                      <div class="col-2">
-                                          <label class="btn btn-outline-success" name="yes-no${i}" for="yes${i}">Yes</label>
-                                      </div>
-                                      <div class="col-3">
-                                      <input type="radio" class="btn-check" name="yes-no${i}" id="no${i}" autocomplete="off">
-                                  </div>
-                                      <div class="col-2">
-                                          <label class="btn btn-outline-success" name="yes-no${i}" for="no${i}">No</label>
-                                          <br><br><br>
-                                          Excitement:
-                                          <select id="${i}">
-                                          <option selected disabled hidden style='display: none' value=''></option>
-                                          <option value="1">1</option>
-                                          <option value="2">2</option>
-                                          <option value="3">3</option>
-                                          <option value="4">4</option>
-                                          <option value="5">5</option>
-                                          </select>
-                                      </div>
-
-                                  </div>
-                              </li>`;
-     container.append(str);
-      console.log("HI");
-     }
-   }
- }
- }
 
  $("#send-button").on("click",function() {
  console.log("HI");
