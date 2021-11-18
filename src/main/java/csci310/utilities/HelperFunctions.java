dc796333d5d77fd9b91ed13afa1c67f04a0e0a3f
Andrew Klotz <aklotz@usc.edu>
2021-11-18 14:43:03 -0800

    TDD for helperfunctions.

---
+++ src/main/java/csci310/utilities/HelperFunctions.java
@@ -0,0 +1,23 @@
+package csci310.utilities;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+
+public class HelperFunctions {
+    public static String get(String urlString) throws IOException {
+        return null;
+    }
+
+    public static Gson shared() {
+        return null;
+    }
+
+    public static String getSalt() {
+        return null;
+    }
+
+    public static String getSecurePassword(String passwordToHash, String salt, String algorithm) {
+        return null;
+    }
+}
