d4cbc9474668395cd0efaf3ca24b2d85204fd800
Andre Hsueh <andrehsu@usc.edu>
2021-11-18 15:32:21 -0500

    Update proposalResponse.js

--- src/main/webapp/js/postload/pages/proposalResponse.js
+++ src/main/webapp/js/postload/pages/proposalResponse.js
@@ -106,7 +106,7 @@ $.ajax({
                                   }),
     contentType: "application/json; charset=utf-8",
     dataType: "json",
-    success: function(data){alert(data);},
+    success: function(data){},
     error: function(errMsg) {
         alert(errMsg);
     }
