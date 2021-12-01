dde6a875c4a94e72ef509fe6040c805eb3c41936
Andrew Klotz <aklotz@usc.edu>
2021-12-01 14:20:58 -0800

    Event list finished (needs sort)

--- src/main/webapp/js/postload/pages/calendar.js
+++ src/main/webapp/js/postload/pages/calendar.js
@@ -4,13 +4,66 @@ document.querySelector("#switchButton").onclick = (e) => {
     if (document.getElementById('switchButton').innerText.includes("List"))
     {
         document.getElementById("switchButton").innerText = document.getElementById("switchButton").innerText.replace("List", "Calendar");
-        document.getElementById("eventList").hidden = true;
-        document.getElementById("eventCalendar").hidden = false;
+        document.getElementById("eventList").hidden = false;
+        document.getElementById("eventCalendar").hidden = true;
     }
     else
     {
         document.getElementById("switchButton").innerText = document.getElementById("switchButton").innerText.replace("Calendar", "List");
-        document.getElementById("eventList").hidden = false;
-        document.getElementById("eventCalendar").hidden = true;
+        document.getElementById("eventList").hidden = true;
+        document.getElementById("eventCalendar").hidden = false;
     }
 }
+
+function addEventToList(list, event)
+{
+    let eventItem = document.createElement("div");
+    eventItem.className = "listItem";
+
+    let eventDate = document.createElement("p");
+    eventDate.className = "subtitle";
+    eventDate.innerHTML = moment(event.date).format("MMMM Do YYYY")
+    eventItem.append(eventDate);
+
+    let eventName = document.createElement("a");
+    eventName.href = event.url;
+    eventName.innerHTML = event.name;
+    eventItem.append(eventName);
+
+    let eventTime = document.createElement("b");
+    eventTime.innerHTML = event.time;
+    eventItem.append(eventTime);
+
+    list.append(eventItem);
+}
+
+function addEventToCalendar(event)
+{
+}
+
+ajaxGet(ENDPOINT_URL + "/proposal/get?type=received&username=" + localStorage.getItem("username"), (response) => {
+      let json = JSON.parse(response);
+      var list = document.getElementById("eventList");
+      for (let i = 0; i < json.data.length; i++)
+      {
+        if (json.data[i].finalizedEventID == null)
+        {
+            for (let j = 0; j < json.data[i].events.length; j++)
+            {
+                addEventToList(list, json.data[i].events[j]);
+                addEventToCalendar(json.data[i].events[j]);
+            }
+        }
+        else
+        {
+            for (let j = 0; j < json.data[i].events.length; j++)
+            {
+                if (json.data[i].events[j].eventID == json.data[i].finalizedEventID)
+                {
+                    addEventToList(list, json.data[i].events[j]);
+                    addEventToCalendar(json.data[i].events[j]);
+                }
+            }
+        }
+      }
+    })
