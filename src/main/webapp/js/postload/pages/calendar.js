11743b158d1c1369e502c7f759fd5bebf1ab7e07
Andrew Klotz <aklotz@usc.edu>
2021-12-01 15:20:45 -0800

    Event list sort finished.

--- src/main/webapp/js/postload/pages/calendar.js
+++ src/main/webapp/js/postload/pages/calendar.js
@@ -1,21 +1,14 @@
-document.querySelector("#switchButton").onclick = (e) => {
-    e.preventDefault()
+var list = document.getElementById("eventList");
 
-    if (document.getElementById('switchButton').innerText.includes("List"))
+function removeChildren(node)
+{
+    while (node.children.length > 0)
     {
-        document.getElementById("switchButton").innerText = document.getElementById("switchButton").innerText.replace("List", "Calendar");
-        document.getElementById("eventList").hidden = false;
-        document.getElementById("eventCalendar").hidden = true;
-    }
-    else
-    {
-        document.getElementById("switchButton").innerText = document.getElementById("switchButton").innerText.replace("Calendar", "List");
-        document.getElementById("eventList").hidden = true;
-        document.getElementById("eventCalendar").hidden = false;
+        node.removeChild(node.firstChild);
     }
 }
 
-function addEventToList(list, event)
+function addEventToList(event)
 {
     let eventItem = document.createElement("div");
     eventItem.className = "listItem";
@@ -34,23 +27,78 @@ function addEventToList(list, event)
     eventTime.innerHTML = event.time;
     eventItem.append(eventTime);
 
+    eventItem.setAttribute("datetime", event.date + "T" + event.time);
+
     list.append(eventItem);
+}
+
+function sortList()
+{
+    var oldestFirst = document.getElementById("listSortSelector").value == "oldestFirst";
+    var sortFunc;
+    if (oldestFirst)
+    {
+        sortFunc = function(event1, event2) {
+            let event1Time = moment(event1.getAttribute("datetime"));
+            let event2Time = moment(event2.getAttribute("datetime"));
+            if (event1Time.isBefore(event2Time))
+            {
+                return -1;
+            }
+            else if (event1Time.isSame(event2Time))
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+    }
+    else
+    {
+        sortFunc = function(event1, event2) {
+            let event1Time = moment(event1.getAttribute("datetime"));
+            let event2Time = moment(event2.getAttribute("datetime"));
+            if (event1Time.isBefore(event2Time))
+            {
+                return 1;
+            }
+            else if (event1Time.isSame(event2Time))
+            {
+                return 0;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+
+    var eventArray = Array.from(list.children);
+    eventArray.sort(sortFunc);
+
+    removeChildren(list);
+    for (let i = 0; i < eventArray.length; i++)
+    {
+        list.append(eventArray[i]);
+    }
 }
 
 function addEventToCalendar(event)
 {
+
 }
 
 ajaxGet(ENDPOINT_URL + "/proposal/get?type=received&username=" + localStorage.getItem("username"), (response) => {
       let json = JSON.parse(response);
-      var list = document.getElementById("eventList");
       for (let i = 0; i < json.data.length; i++)
       {
         if (json.data[i].finalizedEventID == null)
         {
             for (let j = 0; j < json.data[i].events.length; j++)
             {
-                addEventToList(list, json.data[i].events[j]);
+                addEventToList(json.data[i].events[j]);
                 addEventToCalendar(json.data[i].events[j]);
             }
         }
@@ -60,10 +108,32 @@ ajaxGet(ENDPOINT_URL + "/proposal/get?type=received&username=" + localStorage.ge
             {
                 if (json.data[i].events[j].eventID == json.data[i].finalizedEventID)
                 {
-                    addEventToList(list, json.data[i].events[j]);
+                    addEventToList(json.data[i].events[j]);
                     addEventToCalendar(json.data[i].events[j]);
                 }
             }
         }
       }
+      sortList();
     })
+
+document.getElementById("switchButton").onclick = (e) => {
+    if (document.getElementById('switchButton').innerText.includes("List"))
+    {
+        document.getElementById("switchButton").innerText = document.getElementById("switchButton").innerText.replace("List", "Calendar");
+        document.getElementById("eventList").hidden = false;
+        document.getElementById("listSort").hidden = false;
+        document.getElementById("eventCalendar").hidden = true;
+    }
+    else
+    {
+        document.getElementById("switchButton").innerText = document.getElementById("switchButton").innerText.replace("Calendar", "List");
+        document.getElementById("eventList").hidden = true;
+        document.getElementById("listSort").hidden = true;
+        document.getElementById("eventCalendar").hidden = false;
+    }
+}
+
+document.getElementById("listSortSelector").onchange = (e) => {
+    sortList();
+}
