2f46e0ac0e29aef509c02c2ee062773416cb9759
Andre14254 <andrehsu@usc.edu>
2021-12-01 16:46:05 -0800

    update to function

--- src/main/webapp/js/postload/pages/proposalResponse.js
+++ src/main/webapp/js/postload/pages/proposalResponse.js
@@ -21,35 +21,34 @@ function back(){
          }
          else {
            for (let i=0;i<json.data.length;i++) {
+           for (let j=0;j<json.data[i].events.length;j++){
              let str = `<li class="list-group-item">
                                         <div class="row">
                                             <div class="col-12">
-                                                <strong>${json.data[i].proposalTitle}</strong>
+                                                <strong><a href="${json.data[i].events[j].url}">${json.data[i].proposalTitle}</a></strong>
                                                 <br>
-                                                Event Name: ${json.data[i].events[0].name}
+                                                Event Name: ${json.data[i].events[j].name}
                                                 <br>
-                                                Date: ${json.data[i].events[0].date}
+                                                Date: ${json.data[i].events[j].date}
                                                 <br>
-                                                Time: ${json.data[i].events[0].time}
+                                                Time: ${json.data[i].events[j].time}
                                                 <br>
-                                                URL: ${json.data[i].events[0].url}
-                                                <br>
-                                                Genre: ${json.data[i].events[0].genre}
+                                                Genre: ${json.data[i].events[j].genre}
                                         </div>
                                             <div class="col-3">
-                                                <input type="radio" class="btn-check" name="yes-no${i}" id="yes${i}" autocomplete="off">
+                                                <input type="radio" class="btn-check" name="yes-no${i}${j}" id="yes${i}${j}" autocomplete="off">
                                             </div>
                                             <div class="col-1">
-                                                <label class="btn btn-outline-success" name="yes-no${i}" for="yes${i}">Yes</label>
+                                                <label class="btn btn-outline-success" name="yes-no${i}${j}" for="yes${i}${j}">Yes</label>
                                             </div>
                                             <div class="col-3">
-                                                <input type="radio" class="btn-check" name="yes-no${i}" id="maybe${i}" autocomplete="off">
+                                                <input type="radio" class="btn-check" name="yes-no${i}${j}" id="maybe${i}${j}" autocomplete="off">
                                             </div>
                                             <div class="col-1">
-                                                <label class="btn btn-outline-success" name="yes-no${i}" for="maybe${i}">Maybe</label>
+                                                <label class="btn btn-outline-success" name="yes-no${i}${j}" for="maybe${i}${j}">Maybe</label>
                                                 <br><br><br>
                                                   Excitement:
-                                                  <select id="${i}">
+                                                  <select id="${i}${j}">
                                                   <option selected disabled hidden style='display: none' value=''></option>
                                                   <option value="1">1</option>
                                                   <option value="2">2</option>
@@ -59,10 +58,10 @@ function back(){
                                                   </select>
     </div>
                                             <div class="col-3">
-                                            <input type="radio" class="btn-check" name="yes-no${i}" id="no${i}" autocomplete="off">
+                                            <input type="radio" class="btn-check" name="yes-no${i}${j}" id="no${i}${j}" autocomplete="off">
                                         </div>
                                             <div class="col-1">
-                                                <label class="btn btn-outline-success" name="yes-no${i}" for="no${i}">No</label>
+                                                <label class="btn btn-outline-success" name="yes-no${i}${j}" for="no${i}${j}">No</label>
 
                                             </div>
       
@@ -70,6 +69,7 @@ function back(){
                                     </li>`;
            container.append(str);
            }
+         }
          }
        }
        }
@@ -87,15 +87,23 @@ const search = (username, resultsContainer) => {
 var xhReq = new XMLHttpRequest();
 xhReq.open("GET", ENDPOINT_URL + "/proposal/get?type=received&username=" + username, false);
 xhReq.send(null);
-var json = JSON.parse(xhReq.responseText);  
+var json = JSON.parse(xhReq.responseText);
+for (let i=0;i<json.data.length;i++) {
+for (let j=0;j<json.data[i].events.length;j++){
+ if ($('input[name=yes-no'+i+j+']:checked').length > 0 && $("#"+i+j+" option:selected").length > 0) {
+ alert("Please fill out all entries.");
+ return;
+ }
+ }
+ }
  for (let i=0;i<json.data.length;i++) {
+for (let j=0;j<json.data[i].events.length;j++){
+ if ($('input[name=yes-no'+i+j+']:checked').length > 0 && $("#"+i+j+" option:selected").length > 0) {
 
- if ($('input[name=yes-no'+i+']:checked').length > 0 && $("#"+i+" option:selected").length > 0) {
-
- let ele = document.getElementsByName('yes-no'+i);
+ let ele = document.getElementsByName('yes-no'+i+j);
  let availabilityVal;
-             for(let j = 0; j < ele.length; j+=2) {
-                 if(ele[j].checked && j == 0) {
+             for(let k = 0; k < ele.length; k+=2) {
+                 if(ele[k].checked && k == 0) {
                     availabilityVal = 1;
                  }
                  else
@@ -103,11 +111,11 @@ var json = JSON.parse(xhReq.responseText);
                     availabilityVal = 0;
                  }
              }
-   let excitementVal = document.getElementById(""+i).value;
+   let excitementVal = document.getElementById(""+i+j).value;
 $.ajax({
     type: "POST",
     url: "http://localhost:8080/response/send",
-    data: JSON.stringify({ "eventID": ""+json.data[0].events[0].eventID,
+    data: JSON.stringify({ "eventID": ""+json.data[i].events[j].eventID,
                                   "availability": availabilityVal,
                                   "excitement": excitementVal,
                                   "receiverUsername": ""+username
@@ -119,6 +127,7 @@ $.ajax({
         alert(errMsg);
     }
 });
+}
      }
      }
      });
